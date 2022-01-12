50a82fd6259638ade8a6821634e1ca3d414952cb
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-01-12 09:58:01 +0530

    fibonoseries code uploading

---
+++ testHello/testHello/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome");
+            Console.WriteLine("Changes coming from second developer");
+
+            // syntax classname variable=new className();
+            fibonoSeries fibonocci = new fibonoSeries();
+            fibonocci.findfibonoceiSeries();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ testHello/testHello/fibonoSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testHello
+{
+    class fibonoSeries
+    {
+        /// instance variable
+        /// public int first = 0
+        /// static variable
+        /// public static int second = 0
+        
+        public void findfibonoceiSeries()
+        {
+            Console.WriteLine("Please enter how many fibonocci numbers we have to generate");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int first=0,second=1;
+            int result = 0;
+            result = first + second;
+            Console.Write("{0} {1} {2}",first,second,result);
+            for (int i = 3; i < number; i++)
+            {
+                first = second;
+                second = result;
+                result = first + second;
+                Console.Write(" "+result);
+            }
+        }
+        
+    }
+}
