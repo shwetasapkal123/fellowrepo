bec4b3d3c4035332dc3aea9b0b95f59c05bdb720
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-01-15 14:35:47 +0530

    [Shweta] Uploading basic c# practice prgrm

---
+++ practice/OperatorArithmetic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+     class OperatorArithmetic
+    {
+       public static void operatorOperation()
+        {
+            int x, y,sum,sub,mul,modu;
+            float div;
+          
+            Console.WriteLine("Enter first number ");
+            x=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second number ");
+            y=Convert.ToInt32(Console.ReadLine());
+
+            sum = x + y;
+            sub = x - y;
+            mul = x * y;
+            div = x / y;
+            modu = x % y;
+
+            Console.WriteLine("Addition is=" + sum + "Subtraction is=" + sub + " multiplication is=" + mul + "Division is=" + div + "Reminder is=" + modu);
+            
+        }
+       
+       
+    }
+}
---
+++ practice/TypeCasting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+     class TypeCasting
+    {
+        public static void defineTypeCast()
+        {
+            //Implicit Castig is done smaller to larger size type
+            // char->int->long->float->double
+            int myInt = 9;
+            double myDouble = myInt;
+            double myFloat = myDouble;
+            Console.WriteLine("Implicit Casting :"+ myDouble +" "+ myFloat);
+            //Explicit Casting is done manually from larger to smaller by placing type in paranthesis
+            //double->float->long->int->char
+            double myDouble2 = 9.78;
+            int myInt2 = (int)myDouble2;
+            bool myBool = true;
+            Console.WriteLine("explicit type is "+ myInt2);
+            Console.WriteLine(Convert.ToString(myInt));
+            Console.WriteLine(Convert.ToDouble(myInt2));
+            Console.WriteLine(Convert.ToString(myBool));
+        }
+    }
+}
